8ec6ebd7bcc92a70ab0412b0ec454aafd43db0d3
nateshmbhat <nateshmbhat1@gmail.com>
2024-09-30 16:47:04 +0530

    updated minSdk to 23

--- android/src/main/kotlin/com/example/do_not_disturb/DoNotDisturbPlugin.kt
+++ android/src/main/kotlin/com/example/do_not_disturb/DoNotDisturbPlugin.kt
@@ -1,12 +1,16 @@
 package com.example.do_not_disturb
 
-import androidx.annotation.NonNull
-
+import android.app.NotificationManager
+import android.content.Context
+import android.content.Intent
+import android.os.Build
+import android.provider.Settings
 import io.flutter.embedding.engine.plugins.FlutterPlugin
 import io.flutter.plugin.common.MethodCall
 import io.flutter.plugin.common.MethodChannel
 import io.flutter.plugin.common.MethodChannel.MethodCallHandler
 import io.flutter.plugin.common.MethodChannel.Result
+
 
 /** DoNotDisturbPlugin */
 class DoNotDisturbPlugin: FlutterPlugin, MethodCallHandler {
@@ -46,26 +50,20 @@ class DoNotDisturbPlugin: FlutterPlugin, MethodCallHandler {
 
   private fun getDNDStatus(): Int {
     val notificationManager = context.getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
-    return if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      notificationManager.currentInterruptionFilter
-    } else {
-      NotificationManager.INTERRUPTION_FILTER_ALL
-    }
+    return notificationManager.currentInterruptionFilter
   }
 
   private fun openDndSettings() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.Q) {
-      val intent = Intent(Settings.ACTION_ZEN_MODE_SETTINGS)
-      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+      val intent = Intent(Settings.ACTION_ZEN_MODE_PRIORITY_SETTINGS)
+      intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK)
       context.startActivity(intent)
     }
   }
 
   private fun openNotificationPolicyAccessSettings() {
-    if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
-      val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
-      intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
-      context.startActivity(intent)
-    }
+    val intent = Intent(Settings.ACTION_NOTIFICATION_POLICY_ACCESS_SETTINGS)
+    intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK
+    context.startActivity(intent)
   }
 }
